15ecb6f7299b4b75764757886b925b06dd331666
muhhammedabdullah <93083475+muhhammedabdullah@users.noreply.github.com>
2021-10-24 15:30:45 +0200

    Add Solution of Q2

---
+++ Q2.py
@@ -0,0 +1,22 @@
+fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
+               'limes': 0.75, 'strawberries': 1.00}
+def buyLotsOfFruit(orderList):
+    """
+        orderList: List of (fruit, numPounds) tuples
+
+    Returns cost of order
+    """
+    totalCost = 0.0
+    multi=0.0
+    fruit=0.0
+    for order in orderList:
+        fruit=order[0]
+        multi=fruitPrices[fruit]
+        totalCost+=order[1]*multi
+    return totalCost
+orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
+#Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
+print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
+orderList = [('oranges', 4.0), ('strawberries', 3.0), ('limes', 4.0)]
+print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
+#Cost of [('oranges', 4.0), ('strawberries', 3.0), ('limes', 4.0)] is 12.0
